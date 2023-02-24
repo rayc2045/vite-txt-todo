a12be252df6e456ac123e1db9906b2756e880dba
rayc2045 <>
2023-02-25 00:11:19 +0800

    Upload dist files

--- src/scripts/esm/prefer.js
+++
@@ -1,4 +0,0 @@
-export default {
-  darkMode: window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches,
-  motion: window.matchMedia && !window.matchMedia('(prefers-reduced-motion: reduce)').matches
-}
